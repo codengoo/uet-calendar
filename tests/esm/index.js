d5cb4842cf630c9274ffe8c903cd7ecd11a2312d
NghiaDT <nghiadt22.work@gmail.com>
2024-09-20 00:52:24 +0700

    done

---
+++ tests/esm/index.js
@@ -0,0 +1,12 @@
+import UET from "@nghiavuive/uet-calendar"
+
+(async () => {
+  const pipeline = new UET.SubjectPipeline();
+  pipeline.addAggregateBySID();
+
+  const subject = new UET.Subject({name: "Nghia", semester: "041"}, pipeline);
+  const data = await subject.craw();
+
+  console.log(data);
+  
+})()
